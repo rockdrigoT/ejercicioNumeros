d8fd5512d2d1657df7c8cbb58f7f76f4d245e5f4
Rodrigo Torres González <roTorres@MacBook-Pro-de-Rodrigo.local>
2017-05-13 09:42:49 -0500

    Initial Commit

---
+++ prueba/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prueba
+//
+//  Created by Rodrigo Torres González on 13/05/17.
+//  Copyright © 2017 Rodrigo Torres González. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
